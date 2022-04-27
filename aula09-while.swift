4d10c1dabe5a9933bf0b0ac4bc49c9ec92851d4e
Priscilla Correa <99624784+prissycorrea@users.noreply.github.com>
2022-04-27 14:52:51 -0300

    Create aula09-while.swift

---
+++ aula09-while.swift
@@ -0,0 +1,22 @@
+//aula 09 - while e do while
+//dry - don't repeat yourself
+
+//while CONDICAO {
+//    bloco de looping
+//}
+
+//while  - teste no inicio
+print("usando o while")
+var i = 1
+while i <= 10 {
+    print(i)
+    i += 1
+}
+
+//repeat ou do while - faz uma instrucao e depois checa a condicao
+print("usando o repeat")
+var a = 1
+repeat {
+    print(a)
+    a += 1
+} while a <= 10
