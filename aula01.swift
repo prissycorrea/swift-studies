d1ea9b241b4aec39a08c5bc51e9c4af44e13c316
Priscilla Correa <99624784+prissycorrea@users.noreply.github.com>
2022-04-27 14:48:17 -0300

    Create aula01.swift

---
+++ aula01.swift
@@ -0,0 +1,23 @@
+//aula 01 - 02
+
+// var = variavel (mutavel)
+var message = "Olá, mundo!"
+print(message)
+
+message = "iOS"
+print(message)
+
+//let = constantes (nao mutaveis)
+let nani = "Nanis se amam"
+print(nani)
+
+    //tipos de dados
+let int = 1
+let double = 3.14
+let string = "Ola"
+let isDriver = true
+
+let number: Int = 2
+let pi: Double = 3.14
+let name: String = "Ola mundo"
+let no: Bool = false
