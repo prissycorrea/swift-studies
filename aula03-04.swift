35aa1d95488fff6ab4d9891d69df379d5f3cc31e
Priscilla Correa <99624784+prissycorrea@users.noreply.github.com>
2022-04-27 14:49:07 -0300

    Create aula03-04.swift

---
+++ aula03-04.swift
@@ -0,0 +1,43 @@
+//aula 03 - 04
+// converter tipos de dados
+let xx = 10
+let name = String(xx)
+print(name)
+
+//nao recomendavel
+let yy = "10"
+let value = Int(yy)
+print(value)
+
+//casting = converter um tipo de dado para outro tipo de dado
+
+
+//incrementos e operadores logicos
+let result = 2 > 3
+print(result)
+
+let x = 10
+let y = 20
+let res = x != y 
+print(res)
+
+let firstName = "Nani"
+let lastName = "Macio"
+
+firstName == lastName
+
+
+// == != = < <= > >=
+
+// ! && ||
+
+let isDriverr = false
+let isStudent = true 
+
+!isStudent //negacao
+
+
+//incremento
+var z = 11
+z += 1
+z -= 1
